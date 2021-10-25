8e44dbe477438254f736a804db7329cbc14d428c
David Black <david_black_uk@yahoo.com>
2021-10-25 14:44:30 +0100

    INcreased code coverage in shared

---
+++ DesignYourOwnCpu/SharedTests/MockAsciiMapperTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Shared;
+
+namespace SharedTests
+{
+    public class MockAsciiMapperTests
+    {
+        [Test]
+        [TestCase(32, ' ')] // legal to legal
+        [TestCase(27, '.')] // unknown to period
+        public void ConvertByteToChar_whenInvoked_MapsAsExpected(byte from, char expected)
+        {
+            var res = MockAsciiMapper.ConvertByteToChar(from);
+            res.Should().Be(expected);
+        }
+        
+        
+        [Test]
+        [TestCase(' ', 32)] // legal to legal
+        [TestCase('\b', 255)] // unknown to period
+        public void ConvertCharToByte_whenInvoked_MapsAsExpected(char from, byte expected)
+        {
+            var res = MockAsciiMapper.ConvertCharToByte(from);
+            res.Should().Be(expected);
+        }
+    }
+}
