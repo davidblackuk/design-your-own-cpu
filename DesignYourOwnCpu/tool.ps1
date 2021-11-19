467a108abef7982af2f0b9b5910b99c0af58eab7
David Black <david_black_uk@yahoo.com>
2021-11-19 16:18:10 +0000

    Removed compiler for initial release

--- DesignYourOwnCpu/tool.ps1
+++ DesignYourOwnCpu/tool.ps1
@@ -4,7 +4,7 @@
 
 # USAGE:
 #
-#  .\tool <assemble | run | compile> (-quiet) Filename
+#  .\tool <assemble | run > (-quiet) Filename
 #
 #
 #  Examples:
@@ -12,14 +12,12 @@
 #       Assemble .\Asm\sum-two-ints-register-based.asm to .\Asm\sum-two-ints-register-based.bin (and .sym)
 #  .\tool.ps1 run .\Asm\sum-two-ints-register-based.bin
 #       Run the code in the emulator bin file .\Asm\sum-two-ints-register-based.bin
-#  .\tool.ps1 compile .\Asm\average-numbers.p
-#       Compile the pascalish file .\Asm\average-numbers.p and produce .\Asm\average-numbers.asm
 #
 #  .\tool.ps1 assemble -quiet -filename .\Asm\sum-two-ints-register-based.asm
 #       Assemble .\Asm\sum-two-ints-register-based.asm to .\Asm\sum-two-ints-register-based.bin (and .sym)
 #   
 param (
-    # the command to run (assemble, compile or run)
+    # the command to run (assemble,  or run)
     [Parameter(Mandatory=$true)]
     [string]$Command,
 
@@ -35,13 +33,11 @@ param (
 $DotNet = "dotnet.exe"
 $AssemblerFolder = ".\Assembler"
 $EmulatorFolder = ".\Emulator"
-$CompilerFolder = ".\Compiler"
 
 $ExecutableSubFolder="bin\Debug\net5.0"
 
 $Assembler = "$AssemblerFolder\$ExecutableSubFolder\Assembler.exe"
 $Emulator = "$EmulatorFolder\$ExecutableSubFolder\Emulator.exe"
-$Compiler = "$CompilerFolder\$ExecutableSubFolder\Compiler.exe"
 
 
 # we need dotnet installed
@@ -52,9 +48,9 @@ if (-Not (Get-Command $DotNet -ErrorAction SilentlyContinue))
 }
 
 # test we are in the right place and the expected directories exist
-if  ( -Not ((Test-Path $AssemblerFolder) -And (Test-Path $EmulatorFolder) -And (Test-Path $CompilerFolder)))
+if  ( -Not ((Test-Path $AssemblerFolder) -And (Test-Path $EmulatorFolder) ))
 {
-    Write-Error "Missing one of the expected folders relative to this script:  $AssemblerFolder , $EmulatorFolder, $CompilerFolder"
+    Write-Error "Missing one of the expected folders relative to this script:  $AssemblerFolder , $EmulatorFolder"
 }
 
 if  ( -Not (Test-Path $Assembler) )
@@ -69,21 +65,12 @@ if  ( -Not (Test-Path $Emulator) )
     & $DotNet build -v q --nologo "$EmulatorFolder\Emulator.csproj"
 }
 
-if  ( -Not (Test-Path $Compiler) )
-{
-    Write-Host -ForegroundColor Yellow "Building the compiler"
-    & $DotNet build -v q --nologo "$CompilerFolder\Compiler.csproj"
-}
-
-
 
 if ($Quiet) {
     $AppArgs = "--quiet=true"
 } else {
     $AppArgs = ""
 }
-
-Write-Host "$command $Filename $AppArgs"
 
 switch ($Command.ToLowerInvariant()) {
     "assemble"  
@@ -93,11 +80,6 @@ switch ($Command.ToLowerInvariant()) {
     "run"     
     { 
         & $Emulator $AppArgs --input=$Filename 
-    }
-
-    "compile"
-    { 
-        & $Compiler $AppArgs --input=$Filename 
     }
 
 }
