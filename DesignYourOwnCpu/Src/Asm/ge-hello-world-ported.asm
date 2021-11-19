c11c8e56aad0db95da0c10ba78c93fd92517ac87
David Black <david_black_uk@yahoo.com>
2021-11-19 16:03:33 +0000

    Added comments to asm file

--- DesignYourOwnCpu/Src/Asm/ge-hello-world-ported.asm
+++ DesignYourOwnCpu/Src/Asm/ge-hello-world-ported.asm
@@ -3,25 +3,25 @@
 # Adjusted  to use a lower address and our syntax
 #
 
-LD R1, 0x80
-# H
-LD R2, 72
-STL R2, (R1)
-# e
-ADD R1, 1
-LD R2, 101
-STL R2, (R1)
-# l
-ADD R1, 1
-LD R2, 108
-STL R2, (R1)
-# l
-ADD R1, 1
-LD R2, 108
-STL R2, (R1)
-# o
-ADD R1, 1
-LD R2, 111
-STL R2, (R1)
+LD R1, 0x80         # start address to store our string at
+
+LD R2, 72           # load an 'H' character in to R2
+STL R2, (R1)        # store the letter in the address held in R1 (80)
+
+ADD R1, 1           # increment R1 to next memory location 
+LD R2, 101          # load an 'e' character in to R2
+STL R2, (R1)        # store the letter in the address held in R1 (81)
+
+ADD R1, 1           # increment R1 to next memory location 
+LD R2, 108          # load an 'l' character in to R2
+STL R2, (R1)        # store the letter in the address held in R1 (82)
+
+ADD R1, 1           # increment R1 to next memory location
+LD R2, 108          # load an 'l' character in to R2
+STL R2, (R1)        # store the letter in the address held in R1 (83)
+
+ADD R1, 1           # increment R1 to next memory location
+LD R2, 111          # load an 'o' character in to R2
+STL R2, (R1)        # store the letter in the address held in R1 (84)
 
 HALT
